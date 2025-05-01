16d356c911984af743bb619070c66b47b7079371
jeryldev <your.email@example.com>
2025-05-01 19:12:48 +0800

    Create the migration and model for the achievement feature

---
+++ database/migrations/2025_05_01_111110_create_achievements_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('achievements', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('type');
+            $table->string('name');
+            $table->text('description');
+            $table->string('icon');
+            $table->integer('points');
+            $table->boolean('is_unlocked')->default(false);
+            $table->timestamp('unlocked_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('achievements');
+    }
+};
